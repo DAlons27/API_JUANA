da620a87e7bf3548d403fc9df0fcd4251e757c4b
DAlons27 <alonso.morey.arq@gmail.com>
2022-06-20 07:36:20 -0500

    confirmar

--- src/app.py
+++ src/app.py
@@ -1,7 +1,6 @@
-from flask import Flask, jsonify, request
+from flask import Flask, redirect, render_template
 from flask_mysqldb import MySQL
 from config import config
-
 
 from routes.auth import routes_auth
 from dotenv import load_dotenv
@@ -12,22 +11,22 @@ app = Flask(__name__)
 app.register_blueprint(routes_auth, url_prefix="/")
 app.register_blueprint(rutas, url_prefix="/")
 
+
 # La conexion para poder trabajar con la base de datos
 conexion = MySQL(app)
 
 def pagina_no_encontrada(error):
     """
-    It returns a 404 error page
+    Retorna una pagina de error cuando no se encuentra la pagina
     
     :param error: The error that was raised
     """
-    return "<h1>La pagina que intentas buscar no existe ... </h1>", 404
-
+    #return "<h1>La pagina que intentas buscar no existe ... </h1>", 404
+    return render_template("404.html"), 404
 
 if __name__ == '__main__':
     # Recurre al archivo configuracion y toma el parametro DEBUG = TRUE
     app.config.from_object(config['development'])
-    # Manejador de errores, se le pasa como argumento el codigo de respuesta y la funcion
     app.register_error_handler(404, pagina_no_encontrada)
     # Inicia el servidor en el puerto 5000
     app.run(port=5000)
