ab504fff4b63ff6170f62d0ffc824942a9f91b54
DAlons27 <alonso.morey.arq@gmail.com>
2022-06-20 18:37:55 -0500

    agregando funciones

--- src/routes/rutas.py
+++ src/routes/rutas.py
@@ -99,19 +99,20 @@ def trasmitir_estado():
     Permite el registro de informacion en la base de datos por parte de Sharff
     """
     from app import conexion
-    
-    #if (validar_codigo(request.json['numeroGuia'])):
-    try:
-        cursor = conexion.connection.cursor()
-        # Obtengo los datos del json que se le pasa por parametro y los guardo en una variable
-        sql = "INSERT INTO products (numeroGuia, numeroPedido, estado, lugar, quienRecibe, motivoDescripcion, fecha, hora, link, observacion) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}', '{8}', '{9}')".format(
-            request.json['numeroGuia'], request.json['numeroPedido'], request.json['estado'], request.json['lugar'], request.json['quienRecibe'], request.json['motivoDescripcion'], request.json['fecha'], request.json['hora'], request.json['link'], request.json['observacion'])
-        cursor.execute(sql)
-        conexion.connection.commit()  # Confirma la accion de insercion
-        return jsonify({'mensaje': "Se registró correctamente."})
 
-    except Exception as ex:
-        return jsonify({'mensaje': "Error"})
+    if (validar_nPedido(request.json['numeroPedido'])):
+        try:
+            cursor = conexion.connection.cursor()
+            # Obtengo los datos del json que se le pasa por parametro y los guardo en una variable
+            sql = "INSERT INTO products (numeroGuia, numeroPedido, estado, lugar, quienRecibe, motivoDescripcion, fecha, hora, link, observacion) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}', '{8}', '{9}')".format(
+                request.json['numeroGuia'], request.json['numeroPedido'], request.json['estado'], request.json['lugar'], request.json['quienRecibe'], request.json['motivoDescripcion'], request.json['fecha'], request.json['hora'], request.json['link'], request.json['observacion'])
+            cursor.execute(sql)
+            conexion.connection.commit()  # Confirma la accion de insercion
+            return jsonify({'mensaje': "Se registró correctamente."})
+        except Exception as ex:
+            return jsonify({'mensaje': "Error"})
+    else:
+        return jsonify({'mensaje': "No se pudo registrar."})
 
 @rutas.route('/actualizarEstado/<n_pedido>', methods=['PUT'])
 def actualizar_estado(n_pedido):
