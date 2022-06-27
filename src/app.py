982f3571f60301726594d92554f00e0aa5b993c8
DAlons27 <alonso.morey.arq@gmail.com>
2022-06-26 23:45:27 -0500

    Agregando plantillas para errores

--- src/app.py
+++ src/app.py
@@ -22,12 +22,25 @@ def pagina_no_encontrada(error):
     :param error: The error that was raised
     """
     #return "<h1>La pagina que intentas buscar no existe ... </h1>", 404
-    return render_template("404.html"), 404
+    return render_template('errors/404.html'), 404
+
+
+def error_autorizacion(error):
+    """
+    Retorna una pagina de error al no tener autentificacion
+    
+    :param error: The error that was raised
+    """
+    #return "<h1>La pagina que intentas buscar no existe ... </h1>", 404
+    return render_template('errors/405.html'), 405
+
+
 
 if __name__ == '__main__':
     # Recurre al archivo configuracion y toma el parametro DEBUG = TRUE
     app.config.from_object(config['development'])
     # Registro la funcion pagina_no_encontrada como pagina de error
     app.register_error_handler(404, pagina_no_encontrada)
+    app.register_error_handler(405, error_autorizacion)
     # Inicia el servidor en el puerto 5000
     app.run(port=5000)
